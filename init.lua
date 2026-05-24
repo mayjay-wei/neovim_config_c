9785f52d4fea84e1cf1df4bce682dee9bf83ef15
TCWei <eremyj11@gmail.com>
2026-05-24 10:27:38 +0800

    Add clang-format keymap

--- init.lua
+++ init.lua
@@ -7,5 +7,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.tabstop = 4      -- Number of spaces that a <Tab> character visually counts for
 vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of auto-indent
 vim.opt.expandtab = true -- Convert every tab you press into spaces
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = "Format current file with clang-format" })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Show diagnostic error" })
 require("config.lazy")
