d61ad8798416534ba53a6d6655c6886b260c3ccf
TCWei <eremyj11@gmail.com>
2026-05-24 10:25:41 +0800

    Add the indent tab to 4 spaces

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@
 vim.opt.number = true          
 vim.opt.relativenumber = true  
 vim.opt.signcolumn = "yes"
+vim.opt.tabstop = 4      -- Number of spaces that a <Tab> character visually counts for
+vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of auto-indent
+vim.opt.expandtab = true -- Convert every tab you press into spaces
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Show diagnostic error" })
 require("config.lazy")
